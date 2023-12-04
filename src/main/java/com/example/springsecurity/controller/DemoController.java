82f869610bc1fb3020441fcd3bff26ca9381430c
tawhid.hassan <tawhid.tirzok@gmail.com>
2023-12-04 23:14:04 +0600

    changed response

--- src/main/java/com/example/springsecurity/controller/DemoController.java
+++ src/main/java/com/example/springsecurity/controller/DemoController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping("/demo")
     public String demo() {
-        return "hello world";
+        return "hello world!";
     }
 }
